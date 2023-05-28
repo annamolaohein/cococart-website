c4b37dd621497eb3ad636f471acfbed9886dc0ff
annamolaohein <annamo@nexbe.sg>
2023-05-28 20:16:53 +0630

    responsive nav bar

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const faqTitle = document.querySelector(".faq-title");
 const faqContent = document.querySelector(".faq-content");
 const faqIcon = document.querySelector(".faq-icon");
 
+const burger = document.querySelector(".burger");
+const dropdownMenu = document.querySelector(".dropdown");
+
+const nav = document.querySelector("nav");
+
 // faqTitle.onclick = () => {
 //   faqContent.classList.toggle("show");
 //   faqIcon.classList.toggle("rotate-45");
@@ -13,17 +18,26 @@ const faqIcon = document.querySelector(".faq-icon");
 //   faqIcon.classList.toggle("rotate-45");
 // })
 
-faqContainer.addEventListener('click', () => {
+faqContainer.addEventListener("click", () => {
   // console.log(faqContainer.querySelector('.faq-title'));
   // console.log( event.target.closet('article').querySelector('.faq-content));
-  
+
   //event.target=element that we click
 
-  const clickedArticle = event.target.closest('article')
-  const clickedFaqContent= clickedArticle.querySelector('.faq-content')
-  const clickedFaqIcon= clickedArticle.querySelector('.faq-icon')
+  const clickedArticle = event.target.closest("article");
+  const clickedFaqContent = clickedArticle.querySelector(".faq-content");
+  const clickedFaqIcon = clickedArticle.querySelector(".faq-icon");
 
-  clickedFaqContent.classList.toggle('show')
-  clickedFaqIcon.classList.toggle('rotate-45')
+  clickedFaqContent.classList.toggle("show");
+  clickedFaqIcon.classList.toggle("rotate-45");
+});
 
-})
+burger.addEventListener("click", () => {
+  if (dropdownMenu.classList.contains("hidden")) {
+    dropdownMenu.classList.remove("hidden");
+    nav.style.backgroundColor = "white";
+  } else {
+    dropdownMenu.classList.add("hidden");
+    nav.style.backgroundColor = "#FBBE24";
+  }
+});
